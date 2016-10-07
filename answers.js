060c2a7efc89874826993927dba570b44ef527f9
Mat Kitching <gdsm.kitching@gmail.com>
2016-10-07 00:26:11 +0000

    done

--- answers.js
+++ answers.js
@@ -209,3 +209,169 @@ function countChars(string){
 }
 console.log(countChars("Hello World"));
 
+// Starting on week 3 of the bootcamp, we will be learning about databases.
+//Databases are useful for saving data for long periods of time.
+
+// Contrary to a JavaScript program, where all the data disappears when the 
+// program terminates, databases keep their data "forever".
+
+// In this exercise, we’re going to imagine that we are storing people 
+// information in a database, and that we queried our database to retrieve a list of people.
+
+// Our database returns to us an array of people objects, and each of them
+// has a unique ID that the database uses to refer to it.
+
+// Here’s what our person data could look like:
+
+
+// For this exercise, we want to use Array.prototype.reduce to transform our 
+// array of people into an object, keyed with the unique ID.
+
+// The end result should look like this:
+
+// {
+//   "KeXoYg92is": {
+//     "id": "KeXoYg92is",
+//     "firstName": "John",
+//     "lastName": "Smith",
+//     "email": "john@smith.com"
+//   },
+//   "NkALmSWtUp": {
+//     "id": "NkALmSWtUp",
+//     "firstName": "Donald",
+//     "lastName": "Duck",
+//     "email": "don@disney.com"
+//   },
+//   "m7LPbJYSUg": {
+//     "id": "m7LPbJYSUg",
+//     "firstName": "John",
+//     "lastName": "Vader",
+//     "email": "vader@darkside.com"
+//   }
+// }
+
+var people = [
+  {
+    "id": "KeXoYg92is",
+    "firstName": "John",
+    "lastName": "Smith",
+    "email": "john@smith.com"
+  },
+  {
+    "id": "NkALmSWtUp",
+    "firstName": "Donald",
+    "lastName": "Duck",
+    "email": "don@disney.com"
+  },
+  {
+    "id": "m7LPbJYSUg",
+    "firstName": "John",
+    "lastName": "Vader",
+    "email": "vader@darkside.com"
+  }
+]
+// This object could be useful if we are often looking up people by their 
+// unique ID.
+
+// Write a function called peopleById that takes an array of people and returns 
+// an object where each person is keyed by their unique ID.
+
+// You have effectively created what we call an index, not unlike the one you 
+// have in your phonebook.
+
+/*I want to take the array, var people and which is made up
+of obects, and turn it into a one obj with
+multiple obj's assigned 
+
+
+*/
+var people = [
+  {
+    "id": "KeXoYg92is",
+    "firstName": "John",
+    "lastName": "Smith",
+    "email": "john@smith.com"
+  },
+  {
+    "id": "NkALmSWtUp",
+    "firstName": "Donald",
+    "lastName": "Duck",
+    "email": "don@disney.com"
+  },
+  {
+    "id": "m7LPbJYSUg",
+    "firstName": "John",
+    "lastName": "Vader",
+    "email": "vader@darkside.com"
+  }
+]
+
+
+
+
+
+
+function peopleById(theArray){
+    return theArray.reduce(function(acc, personobj){
+      acc[personobj.id ] = personobj;  
+      
+      return acc;
+
+    }, {})
+}
+    
+    console.log(peopleById(people));
+    
+    
+// Expanding on the previous exercise, this time we are going to create an index on
+// first names. Notice how in the previous exercise, each ID was unique. In this case,
+// two people have the same first name.
+
+// We want to create a function called peopleByFirstName that will take an array of
+// people and return something that looks like this:
+
+// {
+//   "John": [
+//     {
+//       "id": "KeXoYg92is",
+//       "firstName": "John",
+//       "lastName": "Smith",
+//       "email": "john@smith.com"
+//     },
+//     {
+//       "id": "m7LPbJYSUg",
+//       "firstName": "John",
+//       "lastName": "Vader",
+//       "email": "vader@darkside.com"
+//     }
+//   ],
+//   "Donald": [
+//     {
+//       "id": "NkALmSWtUp",
+//       "firstName": "Donald",
+//       "lastName": "Duck",
+//       "email": "don@disney.com"
+//     }
+//   ]
+// }
+// You have effectively created an index on first name. This lets you find all people
+// called, say, “John” without having to look through the whole results.
+
+
+function peopleByFirstName(theArray){
+    return theArray.reduce(function(acc, personObj){
+          if (acc[personObj.firstName]){
+             acc[personObj.firstName].push(personObj)
+          }
+      else{
+        acc[personObj.firstName] = [];
+        acc[personObj.firstName].push(personObj);
+      }
+      return acc;
+
+    }, {})
+}
+    
+    console.log(peopleByFirstName(people));
+    
+    
